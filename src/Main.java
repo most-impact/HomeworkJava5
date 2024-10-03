c7f4ade81c468d8895e696ae8733374f22e76e10
Dmitriy Kurilko <dimakurilko0606@mail.ru>
2024-10-03 12:37:56 +0300

    completed tasks

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,73 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Task 1
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println();
+        //Task 2
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+        //Task 3
+        for (int i = 0; i < 17; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+        System.out.println();
+        //Task 4
+        for (int i = 10; i > -11; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+        //Task 5
+        for (int i = 1904; i < 2097; i++) {
+            if (i % 400 == 0) {
+                System.out.println(i + " год является високосным");
+            } else if (i % 4 == 0 && i % 100 != 0) {
+                System.out.println(i + " год является високосным");
+            }
+        }
+
+        System.out.println();
+        //Task 6
+        for (int i = 7; i < 100; i = i + 7) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+        //Task 7
+        for (int i = 1; i < 513; i = i * 2) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+        //Task 8
+        int totalSum = 0;
+        for (int i = 1; i < 13; i++) {
+            totalSum += 29000;
+            System.out.println(i + " месяц, " + "сумма накоплений равна " + totalSum + " рублей");
+        }
+
+        System.out.println();
+        //Task 9
+        totalSum = 0;
+        for (int i = 1; i < 13; i++) {
+            totalSum += 29000;
+            totalSum = (101 * totalSum) / 100;
+            System.out.println(i + " месяц, " + "сумма накоплений равна " + totalSum + " рублей");
+        }
+
+        System.out.println();
+        //Task 1
+        for (int i = 1; i < 11; i++) {
+            System.out.println("2*" + i + "=" + 2 * i);
         }
     }
 }
